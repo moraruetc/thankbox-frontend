34d73129bf7d36ea20e90eda13d1bfc7aea5c68b
dragos moraru <moraru.etc@gmail.com>
2024-07-27 21:16:53 +0300

    import supabase

--- src/components/MessageList.jsx
+++ src/components/MessageList.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState, useRef } from "react";
 import { useLocation } from "react-router-dom";
+import { createClient } from "@supabase/supabase-js";
 
 const supabase_url = "https://cydexirgvhfcdabxhuoy.supabase.co";
 const supabase_key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN5ZGV4aXJndmhmY2RhYnhodW95Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjE4OTU5MTksImV4cCI6MjAzNzQ3MTkxOX0.o61wGQ6Fwz6tSelK2-7pkhhfDkJ9MPzC8h7mdKjn_nY";
